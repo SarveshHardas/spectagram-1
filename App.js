8332b7d67dfc5c1acad3ade543ca60bd65f57937
SarveshHardas <86554291+SarveshHardas@users.noreply.github.com>
2022-03-08 12:09:27 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Componenet} from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import DrawerNavigator from './navigation/drawNavigation';
+import {NavigationContainer} from '@react-navigation/native';
 
 export default class App extends Componenet {
   render()
